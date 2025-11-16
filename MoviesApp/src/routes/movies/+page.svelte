bd34775c36f04dfe3975bcc69f9bdc515e950d28
Adwait Barkale <adwaitbarkale@gmail.com>
2025-11-16 16:09:16 +0530

    Make movie Container scrollable, add pagination to fetch more movies

--- MoviesApp/src/routes/movies/+page.svelte
+++ MoviesApp/src/routes/movies/+page.svelte
@@ -9,10 +9,11 @@
     const API_KEY = "";
     const API_URL = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
     let loading = true;
-    let searchQuery = '';
+    let searchQuery = "";
+    let page = 1;
     // movies is your fetched array from TMDb
-    $: filteredMovies = movies.filter(movie =>
-        movie.title.toLowerCase().includes(searchQuery.toLowerCase())
+    $: filteredMovies = movies.filter((movie) =>
+        movie.title.toLowerCase().includes(searchQuery.toLowerCase()),
     );
 
     const unsubscribe = user.subscribe((value) => {
@@ -37,6 +38,29 @@
             loading = false;
         }
     });
+
+    async function fetchMoreMovies() {
+        page += 1;
+
+        const res = await fetch(
+            `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&page=${page}`,
+        );
+        const data = await res.json();
+        // Append new movies
+        movies = [...movies, ...data.results];
+    }
+
+    function handleScroll(e) {
+        if (searchQuery === "") {
+            const { scrollTop, scrollHeight, clientHeight } = e.target;
+
+            const reachedBottom = scrollTop + clientHeight >= scrollHeight - 10;
+
+            if (reachedBottom) {
+                fetchMoreMovies();
+            }
+        }
+    }
 </script>
 
 <h1>Movies Page</h1>
@@ -51,17 +75,25 @@
         placeholder="Search movies..."
         bind:value={searchQuery}
     />
-    <div class="movies-grid">
-        {#each filteredMovies as movie}
-            <MovieCard {movie} />
-        {/each}
+    <div class="movies-container" on:scroll={handleScroll}>
+        <div class="movies-grid">
+            {#each filteredMovies as movie}
+                <MovieCard {movie} />
+            {/each}
+        </div>
     </div>
 {/if}
 
 <style>
+    .movies-container {
+        height: 80vh; /* the grid will scroll inside 80% of screen height */
+        overflow-y: auto; /* enable vertical scrolling */
+        padding: 10px;
+    }
     .movies-grid {
         display: flex;
         flex-wrap: wrap;
+        gap: 10px;
         /* background-color: red; */
     }
 </style>
